83d9f892069efbbdbc1ca972b1bb638b8a2188f5
kz-oita <taoi.kizuka@gmail.com>
2020-12-15 20:48:58 +0900

    temporary  commit

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
-function appendImages($targetCard,imageResourceArray){
-    imageResourceArray.forEach(element => {
-        $appendImage = $('<img>',element);
-        $appendImage.addClass('character-image');
-        $targetCard.append($appendImage);        
-    });
-}
-
+const numberOfImagesInCard = 8;
+let $card1 = $("#card1");
+let $card2 = $("#card2");
 let imageResources =[
   {'src':'img/yellow-triangle.png'},
   {'src':'img/yellow-square.png'},
@@ -23,21 +18,53 @@ let imageResources =[
   {'src':'img/black-square.png'},
   {'src':'img/black-circle.png'},
 ];
+startGame();
 
-let $card1 = $('#card1');
-let $card2 = $('#card2');
-const numberOfImagesInCard = 8;
-appendImages($card1,pickUpAndRemoveRandomImages(imageResources));
-appendImages($card2,pickUpAndRemoveRandomImages(imageResources));
+function startGame(){
+  let card1Images = pickUpAndRemoveRandomImages(imageResources);
+  let card2Images = pickUpAndRemoveRandomImages(imageResources);
+
+  let answerIndex = Math.floor(Math.random() * imageResources.length);
+  let answerImage = imageResources[answerIndex];
+  answerImage['class'] = 'answer';
+
+  let card1AnswerIndex = Math.floor(Math.random() * (card1Images.length + 1));
+  card1Images.splice(card1AnswerIndex,0,answerImage);
+
+  let card2AnswerIndex = Math.floor(Math.random() * (card2Images.length + 1));
+  card2Images.splice(card2AnswerIndex,0,answerImage);
+
+  appendImage($card1,card1Images);
+  appendImage($card2,card2Images);
+
+  $('#game-field .answer').click(function(){
+    alert('正解');
+  });
+
+  card1Images = [];
+  card2Images = [];
+  startGame();
+
+}
 
 
 
-function pickUpAndRemoveRandomImages(targetImageresourceArray){
+
+
+function appendImage($targetCard,imageResourceArray){
+  imageResourceArray.forEach(element => {
+      $appendImage = $('<img>',element);
+      $appendImage.addClass('character-image');
+      $targetCard.append($appendImage);
+  });
+}
+
+function pickUpAndRemoveRandomImages(targetImageRersourceArray){
   let returnArray = [];
-  for(let i=0 ; i<numberOfImagesInCard -1; i++){
-    let randomIndex = Math.floor(Math.random() * targetImageresourceArray.length);
-    returnArray.push(targetImageresourceArray[randomIndex]);
-    targetImageresourceArray.splice(randomIndex,1);
+  for(let i=0;i<numberOfImagesInCard -1;i++){
+      let randomIndex = Math.floor( (Math.random() * targetImageRersourceArray.length));
+      returnArray.push(targetImageRersourceArray[randomIndex]);
+      targetImageRersourceArray.splice(randomIndex,1);
   }
   return returnArray;
 }
